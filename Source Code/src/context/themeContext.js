cc5dd0d606b44f759aa27027fdc5ff6dd497a8a7
NasterVill <skatrr@gmail.com>
2018-12-01 23:28:32 +0300

    Some styles were adjusted

--- Source Code/src/context/themeContext.js
+++ Source Code/src/context/themeContext.js
@@ -4,7 +4,7 @@ export const themes = {
     light: {
         id: 'light',
         primary: '#ffffff',
-        secondary: '#6a6a6a',
+        secondary: '#9c9c9c',
         primaryText: '#414141',
         viewComponentsColor: '#1f1f1f',
         secondaryText: '#3f3f3f',
@@ -13,7 +13,7 @@ export const themes = {
         id: 'dark',
         primary: '#232323',
         secondary: '#6a6a6a',
-        viewComponentsColor: '#e9e9e9',
+        viewComponentsColor: '#c8c8c8',
         primaryText: '#dddddd',
         secondaryText: '#bfbfbf',
     },
